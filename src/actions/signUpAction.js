8add288c91e35ec12af4c2598f200b6ad9b32894
Ben Davenport <benjamin.p.davenport@gmail.com>
2020-06-13 17:17:16 -0400

    update signup

--- src/actions/signUpAction.js
+++ src/actions/signUpAction.js
@@ -1,16 +1,15 @@
-// import axios from 'axios';
+import axios from 'axios';
 
 export default(data)=>{
-    console.log('signupAction:')
-    console.log(data)
+    // console.log('signupAction:')
+    // console.log(data)
 
     // for later
-    // const signUpPost = `${window.apiHost}/users/signup`;
-    // const axiosResponse = axios.post(signUpPost, data);
-
+    const signUpPost = `${window.apiHost}/users/signup`;
+    const axiosResponse = axios.post(signUpPost, data);
     return{
         type: "signup",
         payload: data,
-        //payload: axiosResponse
+        payload: axiosResponse
     }
 };
